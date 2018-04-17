509f5f1fbca21e50acce81ad522dff66c653e415
hmanoel <hmanoel47@gmail.com>
2018-04-17 16:59:38 -0300

    Perfeito

--- Tutorial6/AndroidQrCodeScanner/app/src/main/java/com/example/alunos/androidqrcodescanner/MainActivity.java
+++ Tutorial6/AndroidQrCodeScanner/app/src/main/java/com/example/alunos/androidqrcodescanner/MainActivity.java
@@ -1,61 +1,98 @@
 package com.example.alunos.androidqrcodescanner;
 
 import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+
 import android.view.View;
+
 import android.widget.Button;
+
 import android.widget.TextView;
+
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
+
 import com.google.zxing.integration.android.IntentResult;
 
 import org.json.JSONException;
+
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    //Inteface
-    private Button btScan;
-    private TextView lblNome, lblEndereco;
-    //Scanner
-    private IntentIntegrator qrScan;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+
+    private Button bt_scan;
+    private TextView rtl_name, rtl_address;
+    private IntentIntegrator code_qr;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btScan = findViewById(R.id.bt_scan);
-        lblNome = findViewById(R.id.lbl_nome);
-        lblEndereco = findViewById(R.id.lbl_endereco);
-        qrScan = new IntentIntegrator(this);
-        btScan.setOnClickListener(this);
+        bt_scan = findViewById(R.id.bt_scan);
+        rtl_name = findViewById(R.id.rtl_name);
+        rtl_address = findViewById(R.id.rtl_address);
+        code_qr = new IntentIntegrator(this);
+        bt_scan.setOnClickListener(this);
+
     }
 
     @Override
-    public void onClick(View view) {
-        qrScan.initiateScan();
+    public void onClick(View view)
+    {
+
+        code_qr.initiateScan();
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if(result != null){
-            if(result.getContents() == null){
-                Toast.makeText(this, "Result not found", Toast.LENGTH_LONG).show();
-            }else{
-                try {
-                    JSONObject obj = new JSONObject(result.getContents());
-                    lblNome.setText(obj.getString("name"));
-                    lblEndereco.setText(obj.getString("address"));
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
 
-                }catch (JSONException e){
-                    e.printStackTrace();
-                    Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
-                }
-            }
-        }else{
+        IntentResult aftermath = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if(aftermath == null)
+        {
+
             super.onActivityResult(requestCode, resultCode, data);
+
+        }
+        else
+        {
+
+            if(!(aftermath.getContents() == null))
+            {
+
+                try
+                {
+
+                    JSONObject code = new JSONObject(aftermath.getContents());
+                    rtl_name.setText(code.getString("name"));
+                    rtl_address.setText(code.getString("address"));
+
+                }
+                catch (JSONException e)
+                {
+
+                    e.printStackTrace();
+                    Toast.makeText(this, aftermath.getContents(), Toast.LENGTH_LONG).show();
+
+                }
+
+            }
+            else
+            {
+
+                Toast.makeText(this, "Result not found", Toast.LENGTH_LONG).show();
+
+            }
+
         }
     }
+
 }
